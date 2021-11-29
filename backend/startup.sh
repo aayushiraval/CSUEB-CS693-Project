e898cfc848401860a2ba33ad5659af9bffb5f62c
aayushiraval <araval3@horizon.csueastbay.edu>
2021-11-28 20:17:46 -0500

    add ports

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Starting API server..."
-gunicorn app.services.api.wsgi
+gunicorn --reload --workers=1 --timeout=60 --access-logfile=- --worker-class=gevent --bind=0.0.0.0:80 app.services.api.wsgi
